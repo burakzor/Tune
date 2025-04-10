c4e0664125a91234087b29ee49835406b89ebb0d
emre <ykpmrclb06@gmail.com>
2025-04-10 17:04:22 +0300

    Unnecessary changes in Home Frame

--- demo1/src/main/java/com/example/demo/HomeFrame.java
+++ demo1/src/main/java/com/example/demo/HomeFrame.java
@@ -2,22 +2,31 @@ package com.example.demo;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 public class HomeFrame extends Scene {
 
     private Label label;
+    private ImageView messiImageView;
 
 
     public HomeFrame() {
-        super(new VBox(), 1000, 700);
+        super(new VBox(), 800, 600);
 
         Label label = new Label("HOME PAGE");
+
+        Image backgroundImage = new Image(getClass().getResourceAsStream("/com/example/demo/messi.jpeg"));
+        messiImageView = new ImageView(backgroundImage);
+        messiImageView.setFitWidth(800);
+        messiImageView.setFitHeight(600);
+        messiImageView.setPreserveRatio(false);
 
 
         VBox layout = (VBox) getRoot();
         layout.setSpacing(20);
-        layout.getChildren().addAll(label);
+        layout.getChildren().addAll(messiImageView, label);
     }
 
 }
